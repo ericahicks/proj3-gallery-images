f00f831dd43bc7d0629390a62511d5b2265313dc
Erica Hicks <48265629+ericahicks@users.noreply.github.com>
2022-08-11 11:49:17 -0400

    fixing repository and service classes

--- src/main/java/com/skillstorm/lit/data/GalleryImageRepository.java
+++ src/main/java/com/skillstorm/lit/data/GalleryImageRepository.java
@@ -1,5 +1,16 @@
 package com.skillstorm.lit.data;
 
-public class GalleryImageRepository {
+import java.util.List;
+import java.util.UUID;
 
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.skillstorm.lit.models.GalleryImage;
+
+@Repository
+public interface GalleryImageRepository extends CrudRepository<GalleryImage, UUID> {
+
+	List<GalleryImage> findByListingDetail();
+	
 }
