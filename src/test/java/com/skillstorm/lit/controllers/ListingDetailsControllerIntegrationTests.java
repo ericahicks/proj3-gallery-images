cd619879f526d9170d139acb1754a37b494b004a
Erica Hicks <48265629+ericahicks@users.noreply.github.com>
2022-08-18 16:59:10 -0400

    debugged listing details integration test find by id

--- src/test/java/com/skillstorm/lit/controllers/ListingDetailsControllerIntegrationTests.java
+++ src/test/java/com/skillstorm/lit/controllers/ListingDetailsControllerIntegrationTests.java
@@ -7,7 +7,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.sql.Date;
+import java.util.HashSet;
 import java.util.UUID;
+
+import javax.net.ssl.SSLEngineResult.Status;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -20,7 +23,9 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.skillstorm.lit.models.GalleryImage;
 import com.skillstorm.lit.models.ListingDetails;
 import com.skillstorm.lit.services.ListingDetailsService;
 
@@ -38,87 +43,51 @@ public class ListingDetailsControllerIntegrationTests {
 	@Autowired
 	private ListingDetailsService service;
 	
-	private static String url;
-//	private ListingDetails details;
-//	private UUID id;
-//	
-//	@BeforeAll
-//	public static void init() {
-//		url =  "/listing-details/v1/";
-//	}
-//	
-//	@BeforeEach
-//	public void setup() {
-//		id = UUID.randomUUID();
-//		details = new ListingDetails();
-//		details.setId(id);
-//		details.setDescription("A stuffed mouse cat toy.");
-//		details.setMaterial("Synthetic fur and cotton stuffing.");
-//		details.setWeightDescription("4oz");
-//		details.setReleaseDate(new Date(System.currentTimeMillis()));
-//	}
-//	
-	@Test 
-	public void testUpdateDetailsFound() throws Exception {
-
-		///////////////////////////////////////////////////////////////////////
-		//////////////////////////// TRY #2 ///////////////////////////////////
-//		ListingDetails details = new ListingDetails();
-//		details = this.service.save(details);
-//		details.setDescription("Stuffed dinosaur cat toy");
-//		
-//		// details should now be different than the one in the db
-//		ListingDetails foundDetails = this.service.findById(details.getId());
-//		assertNotEquals(foundDetails.getDescription(), details.getDescription());
-//		
-//		//perform the update
-//		String url = "/gallery/v1/" + details.getId();
-//		this.mockMvc.perform(MockMvcRequestBuilders.put(url + details.getId().toString())
-//				.contentType("application/json")
-//	            .content(mapper.writeValueAsString(details))
-//	            .accept("application/json"))
-//				.andExpect(status().isOk())
-//				.andExpect(content().string(mapper.writeValueAsString(details)));
-//		
-//		// image matches the one in the db after updating
-//		foundDetails = this.service.findById(details.getId());
-//		assertEquals(foundDetails.getDescription(), details.getDescription());
-//		
-//		this.service.delete(details.getId());
+	private static String url = "/listing-details/v1/";
+	
+	@Test
+	public void testFindById() throws JsonProcessingException, Exception {
+		// put details in database to find by id
+		ListingDetails details = new ListingDetails();
+		details.setDescription("Park bench made from repurposed wood.");
+		details.setMaterial("wood");
+		details.setReleaseDate(new Date(System.currentTimeMillis()));
+		details.setGalleryImages(new HashSet<GalleryImage>());
+		details = service.save(details);
 		
-		/////////////////////////////////////////////////////////////
-		///////////////////// TRY #1 ////////////////////////////////
-		// create details
-//		ListingDetails savedDetails = service.save(details);
-//		
-//		// update details
-//		details.setDescription("A stuffed bird cat toy.");
-//		details.setMaterial("Synthetic featherse and cotton stuffing.");
-//		details.setWeightDescription("3oz");
-//		
-//		// details should now be different than the one in the db
-//		ListingDetails foundDetails = service.findById(savedDetails.getId());
-//		assertNotEquals(foundDetails.getDescription(), details.getDescription());
-//		assertNotEquals(foundDetails.getMaterial(), details.getMaterial());
-//		assertNotEquals(foundDetails.getWeightDescription(), details.getWeightDescription());
-//		
-//		// perform update
-//		details.setId(savedDetails.getId());
-//		mockMvc.perform(MockMvcRequestBuilders.put(url + savedDetails.getId())
-//					.contentType(MediaType.APPLICATION_JSON)
-//					.content(mapper.writeValueAsString(details))
-//					.accept(MediaType.APPLICATION_JSON))
-//				.andExpect(status().isOk())
-//				.andExpect(content().string(mapper.writeValueAsString(details)));
-//		
-//		// image matches the one in the db after udating
-//		foundDetails = service.findById(savedDetails.getId());
-//		assertThat(foundDetails).usingRecursiveComparison().isEqualTo(savedDetails);
-//		
-		// clean up the db
-//		service.delete(savedDetails.getId());
+		// find by id using mock GET request
+		this.mockMvc.perform(MockMvcRequestBuilders.get(url + details.getId())
+				.accept("application/json"))
+					.andExpect(status().isOk())
+					.andExpect(content().string(mapper.writeValueAsString(details)));
+		
+		// clean up database
+		service.delete(details.getId());
 	}
 	
+	@Test
+	public void testFindAll() {
+		
+	}
 	
+	@Test
+	public void testCreate() {
+		
+	}
+	
+	@Test
+	public void testUpdate() {
+		
+	}
+	
+	@Test
+	public void testDelete() {
+		
+	}
+	
+	@Test
+	public void testDeleteWithImages() {
+		
+	}
 
 }
